d71607cd1a556ebd6dbb10bddbce19769384869e
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-08-28 23:24:23 +0500

    Update task7.py

--- LABS/Lab_2/task7.py
+++ LABS/Lab_2/task7.py
@@ -1,1 +1,11 @@
+from statistics import mean 
+temp = [20,21,22,23,24,25,26,27,28,29,30,11,12,13,14,15,16,17,18,19,1,2,3,4,5,6,7,8,9,10]
 
+print(f"Average: {mean(temp)}")
+print(f"Highest: {max(temp)}, Lowest: {min(temp)} ")
+temp.sort()
+print(f"Sorted Temperature: {temp}")
+
+day = int (input("Enter day number to remove temp: "))
+temp.remove(temp[day-1])
+print(f"After removing day {day} Temperature: {temp}")
