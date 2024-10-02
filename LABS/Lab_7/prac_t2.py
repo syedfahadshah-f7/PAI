d5d69effe1829a4e4ecd9d7144a5bb9a9ea4f422
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-10-02 10:28:01 +0500

    Update prac_t2.py

--- LABS/Lab_7/prac_t2.py
+++ LABS/Lab_7/prac_t2.py
@@ -1,14 +1,5 @@
 import pandas as pd
-df = pd.read_csv("heart.csv")
+df = pd.read_csv("iris.csv")
 
-df.rename(columns =  {'sex' : 'gender'}, inplace = True)
-df['gender'] = df['gender'].replace( {0: 'Female' , 1 :'Male'})
-grouped = df.groupby('gender')
-print(grouped['chol'].mean())
-print(grouped['chol'].max())
-print(grouped['chol'].min())
-
-print(grouped[['chol', 'oldpeak', 'restecg']].mean())
-print(grouped[['chol', 'oldpeak', 'restecg']].min())
-print(grouped[['chol', 'oldpeak', 'restecg']].max())
+print(df)
 
