e844016d1d8a4b9eab8262c2653fd031c6c93a62
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-10-02 10:10:07 +0500

    Update prac_t2.py

--- LABS/Lab_7/prac_t2.py
+++ LABS/Lab_7/prac_t2.py
@@ -1,8 +1,14 @@
 import pandas as pd
 df = pd.read_csv("heart.csv")
-# print(df)
+
 df.rename(columns =  {'sex' : 'gender'}, inplace = True)
 df['gender'] = df['gender'].replace( {0: 'Female' , 1 :'Male'})
 grouped = df.groupby('gender')
 print(grouped['chol'].mean())
+print(grouped['chol'].max())
+print(grouped['chol'].min())
+
+print(grouped[['chol', 'oldpeak', 'restecg']].mean())
+print(grouped[['chol', 'oldpeak', 'restecg']].min())
+print(grouped[['chol', 'oldpeak', 'restecg']].max())
 
