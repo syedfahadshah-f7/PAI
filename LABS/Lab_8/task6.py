e34671326bb7030dac6882a4b4c333325a989f03
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-10-09 18:59:38 +0500

    Update task6.py

--- LABS/Lab_8/task6.py
+++ LABS/Lab_8/task6.py
@@ -1,18 +1,10 @@
 import numpy as np
 
-arr1 = np.arange(1,16)
-a = arr1.reshape(5,3)
+arr = np.arange(1,16)
+arr = arr1.reshape(5,3)
 
-arr2 = np.arange(1,7)
-b = arr2.reshape(3,2)
+brr = np.arange(1,7)
+brr = arr2.reshape(3,2)
 
-def mul(x,y):
-    return x*y
+crr = np.multiply(arr,brr) # error, cannot multiply due to  size
 
-mymul = np.frompyfunc(mul,2,1)
-
-c = mymul(a,b)
-
-print(a,'\n')
-print(b,'\n')
-print(c,'\n') # error, cannot multiply due to  size
