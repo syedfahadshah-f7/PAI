d10c9a47899cea9104106bd459b04dc93e319691
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-10-09 18:57:39 +0500

    Update task5.py

--- LABS/Lab_8/task5.py
+++ LABS/Lab_8/task5.py
@@ -1,16 +1,12 @@
 import numpy as np
 
 arr = np.random.choice([2,5,9,10],size = (4,4))
+print("Array \n",arr,'\n')
+arr = np.multiply(arr,np.eye(4,4))
+print("Array after multiplying Identity Matrix \n",arr,'\n')
+newarr = np.arange(1,32,2)
+newarr = newarr.reshape(4,4)
+print("Odd Values Array \n",newarr,'\n')
+brr = np.add(arr,newarr)
+print("After adding both arrays \n",brr,'\n')
 
-a = np.multiply(arr,np.eye(4,4))
-
-arr2 = np.arange(1,32,2)
-
-b = arr2.reshape(4,4)
-
-c = np.add(a,b)
-
-print(arr,'\n')
-print(a,'\n')
-print(b,'\n')
-print(c,'\n')
