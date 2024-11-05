8d89ed8459650812c28e791186b76fa69bd75e43
Syed Fahad Shah <142867460+syedfahadshah-f7@users.noreply.github.com>
2024-11-05 05:45:35 +0500

    Update Q4.py

--- Assignments/A2/Q4.py
+++ Assignments/A2/Q4.py
@@ -3,4 +3,4 @@ import numpy as np
 arr = [2,4,6,8,10]
 brr = [1,3,5,7,9]
 
-print(f"Sum: {np.add(arr,brr)} Sub: {np.subtract(arr,brr)} Mul: {np.multiply(arr,brr)}")
+print(f"Sum: {np.add(arr,brr)} Sub: {np.subtract(arr,brr)} Mul: {np.multiply(arr,brr)}, Div:{np.div(arr,brr)}")
